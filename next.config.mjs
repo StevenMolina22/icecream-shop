7aa7ad53872fd5e0f67f471d6584066205ce25b4
Marlon Stiven Molina Buitrago <113540687+StevenMolina22@users.noreply.github.com>
2024-05-15 10:31:07 -0300

    Update next.config.mjs for Image allow hosts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images:{
+    remotePatterns: [{hostname:"localhost"}, ],
+  }};
 
 export default nextConfig;
