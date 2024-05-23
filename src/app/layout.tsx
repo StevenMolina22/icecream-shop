b00db5751232cb9f01c5ed130ecc1eafb73273f2
Steven Molina <steven.molina.slayer@gmail.com>
2024-05-23 11:28:45 -0300

    New media

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
